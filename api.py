f3a2fb08e37e5fa7823c1f5f40e02e5e3f0c90d3
djogikan <djogikan@gmail.com>
2022-07-12 10:31:40 +0600

    designed new sections

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-
+from flask_qrcode import QRcode
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
+QRcode(app)
 if __name__ == "__main__":
     
     app.run(debug=True, host="0.0.0.0")
